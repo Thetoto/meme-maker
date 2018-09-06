53849b1ac3cacc99d008089ac4807850ad04f4c8
Thetoto <thomas.lupin@epita.fr>
2018-09-06 23:00:42 +0200

    some changes to fit my needs

--- meme-maker.js
+++ meme-maker.js
@@ -1,6 +1,5 @@
-let fs = require('fs')
-let gm = require('gm').subClass({ imageMagick: true });
-
+let fs = require('fs'); 
+let gm = require('gm').subClass({imageMagick: true});
 module.exports = function(options, callback) {
 
   // Check if image option is set
@@ -27,7 +26,7 @@ module.exports = function(options, callback) {
 
     // Create new graphicsmagick instance
     let img = gm(options.image)
-
+    console.log(img);
     if(!img) {
       return callback(new Error('Can not open this img.'), null)
     }
@@ -42,26 +41,43 @@ module.exports = function(options, callback) {
     const STROKE_COLOR = 'strokeColor' in options ? options.strokeColor : '#000'
     const STROKE_WEIGHT = 'strokeWeight' in options ? options.strokeWeight : 2
     const PADDING = 'padding' in options ? options.padding : 40
-
+    
     // Get the image size to calculate top and bottom text positions
     img.size(function(err, dimensions) {
-      if(!dimensions) {
-        return callback(new Error('Can not open this img.'), null)
+      if(!dimensions || err) {
+        console.error(err);
+        return callback(new Error('Can not open this img or binaries not found.'), null)
       }
-      // Set text position for top and bottom
-      const TOP_POS = Math.abs((dimensions.height / 2) - PADDING) * -1
-      const BOTTOM_POS = (dimensions.height / 2) - PADDING
+      img.orientation(function(err, orient){
+        console.log(orient);
+              // Set text position for top and bottom
+        let ratio = dimensions.width / dimensions.height;
+        let newwidth = 600;
+        let newheight = newwidth / ratio;
+        let TOP_POS = Math.abs((newheight / 2) - PADDING) * -1;
+        let BOTTOM_POS = (newheight / 2) - PADDING;
+        
+        if (orient == "LeftTop" || orient == "LeftBottom" || orient == "`RightTop"  || orient == "RightBottom") {
+          ratio = dimensions.height / dimensions.width;
+          newheight = newwidth / ratio;
+          TOP_POS = Math.abs((newheight / 2) - PADDING) * -1;
+          BOTTOM_POS = (newheight / 2) - PADDING;
+        }
 
-      // Write text on image using graphicsmagick
-      img.font(FONT, FONT_SIZE)
-         .fill(FONT_FILL)
-         .stroke(STROKE_COLOR, STROKE_WEIGHT)
-         .drawText(0, TOP_POS, TOP_TEXT, TEXT_POS)
-         .drawText(0, BOTTOM_POS, BOTTOM_TEXT, TEXT_POS)
-         .write(options.outfile, function(err) {
+
+        // Write text on image using graphicsmagick
+        img.autoOrient()
+          .resize(newwidth)
+          .font(FONT, FONT_SIZE)
+          .fill(FONT_FILL)
+          .stroke(STROKE_COLOR, STROKE_WEIGHT)
+          .drawText(0, TOP_POS, TOP_TEXT, TEXT_POS)
+          .drawText(0, BOTTOM_POS, BOTTOM_TEXT, TEXT_POS)
+          .write(options.outfile, function(err) {
            if (err) return callback(new Error('Failed to save meme: ' + err), null)
            return callback(null)
          })
+      })
     })
   })
 }
